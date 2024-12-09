ed4a94d83f04649071e06c2c29daa6a2f8b668b7
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-12-09 19:48:51 +0800

    removed unused and unnecessary libraries

--- stock/aapl_analysis.R
+++ stock/aapl_analysis.R
@@ -1,15 +1,11 @@
-# Load libraries (some unnecessary ones included)
 if (!require("quantmod")) install.packages("quantmod", dependencies = TRUE)
 if (!require("ggplot2")) install.packages("ggplot2", dependencies = TRUE)
 if (!require("dplyr")) install.packages("dplyr", dependencies = TRUE)
-if (!require("tidyr")) install.packages("tidyr", dependencies = TRUE)
-if (!require("reshape2")) install.packages("reshape2", dependencies = TRUE)
 
 library(quantmod)
 library(ggplot2)
 library(dplyr)
-library(tidyr)
-library(reshape2)
+
 
 fetch_stock_data <- function() {
   getSymbols("AAPL", src = "yahoo", from = "2020-01-01", to = "2023-12-31", auto.assign = TRUE)
@@ -96,5 +92,4 @@ merged_data <- merge_and_compare(stock_data, search_data)
 
 # Step 4: Create graphs
 create_overengineered_graphs(merged_data)
-
 
