26f9abce74a24cb17ab1e72f18bed03edb3b6263
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-12-09 17:35:49 +0800

    Initialize R Scripts and all required libraries

---
+++ stock/aapl_analysis.R
@@ -0,0 +1,12 @@
+# Load libraries (some unnecessary ones included)
+if (!require("quantmod")) install.packages("quantmod", dependencies = TRUE)
+if (!require("ggplot2")) install.packages("ggplot2", dependencies = TRUE)
+if (!require("dplyr")) install.packages("dplyr", dependencies = TRUE)
+if (!require("tidyr")) install.packages("tidyr", dependencies = TRUE)
+if (!require("reshape2")) install.packages("reshape2", dependencies = TRUE)
+
+library(quantmod)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(reshape2)
